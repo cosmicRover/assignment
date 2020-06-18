cb549e963181fdd98eb85ff33474eed82dce309f
joypaul <jpaul3250@gmail.com>
2020-06-18 01:06:06 -0400

    minor cleanup

--- Assignment/Views/ImageQueryViewController.swift
+++ Assignment/Views/ImageQueryViewController.swift
@@ -20,8 +20,6 @@ class ImageQueryViewController: UIViewController {
         return cv
     }()
 
-    let loadingSpinner = UIActivityIndicatorView(style: .large)
-
     // MARK: - ViewModel
 
     let imageQueryViewModel = ImageQueryViewModel()
@@ -70,9 +68,9 @@ class ImageQueryViewController: UIViewController {
         let nav = navigationController
         navigationItem.title = "Search.."
         nav!.navigationBar.tintColor = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
-        self.navigationController!.navigationBar.titleTextAttributes = [.foregroundColor: #colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1)]
-        self.navigationController!.navigationBar.barStyle = .default
-        self.navigationController!.navigationBar.isTranslucent = true
+        navigationController!.navigationBar.titleTextAttributes = [.foregroundColor: #colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1)]
+        navigationController!.navigationBar.barStyle = .default
+        navigationController!.navigationBar.isTranslucent = true
         nav?.navigationBar.prefersLargeTitles = true
         displaySearchButton(shouldDisplay: true)
     }
